3f7d73f0a06fc04c1bc6b254d849fe9debbaf800
Luciano <54756923+lucianovictor@users.noreply.github.com>
2022-03-23 11:01:24 -0300

    :lipstick: Add colors and fonts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -16,7 +16,7 @@ export default class MyDocument extends Document {
           <meta name="author" content="Luciano Victor" />
 
           <link
-            href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Open+Sans:wght@400;700&display=swap"
             rel="stylesheet"
           />
         </Head>
