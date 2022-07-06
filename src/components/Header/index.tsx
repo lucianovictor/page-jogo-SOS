4e88c233c5daeb516406a7bfcb719764e58a305a
Luciano Victor <lucianocarvallho7@gmail.com>
2022-07-06 12:03:32 -0300

    feat: adding method to switch language

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,33 +3,64 @@ import { FaChevronDown } from 'react-icons/fa'
 
 import styles from './styles.module.scss'
 
+type Language = 'eng' | 'rus' | 'fra' | 'deu' | 'pt-br'
+
+const LANGUAGES: Language[] = ['eng', 'rus', 'fra', 'deu', 'pt-br']
+
 export const Header = () => {
-  const [activeLanguages, setActiveLanguages] = useState<boolean>(false)
+  const [activeLanguage, setActiveLanguage] = useState<Language>('eng')
+  const [toggleLanguages, setToggleLanguages] = useState<boolean>(false)
+
+  const handleChangeLanguage = (language: Language) => {
+    setActiveLanguage(language)
+    setToggleLanguages(false)
+  }
+
   return (
     <header className={styles.container}>
-      <img src="logo.png" alt="Logo Sos" />
+      <img src="logo.png" alt="Logo SOS" />
+
       <nav className={styles.navigation}>
         <ul>
-          <li>MAIN</li>
-          <li>about</li>
-          <li>game features</li>
-          <li>System requirements</li>
-          <li>quotes</li>
+          <li>
+            <a href="">MAIN</a>
+          </li>
+          <li>
+            <a href="#section-about">about</a>
+          </li>
+          <li>
+            <a href="#section-features">game features</a>
+          </li>
+          <li>
+            <a href="#section-system-requirements">System requirements</a>
+          </li>
+          <li>
+            <a href="#section-quotes">quotes</a>
+          </li>
         </ul>
       </nav>
+
       <div className={styles['navigation-right']}>
         <div className={styles.languages}>
           <button
             type="button"
-            onClick={() => setActiveLanguages(!activeLanguages)}
+            onClick={() => setToggleLanguages(!toggleLanguages)}
           >
-            ENG
-            <FaChevronDown />{' '}
+            {activeLanguage} <FaChevronDown />
           </button>
-          <ul className={activeLanguages ? styles.active : ''}>
-            <li>RUS</li>
-            <li>FRA</li>
-            <li>DEU</li>
+
+          <ul className={toggleLanguages ? styles.active : ''}>
+            {LANGUAGES.map(language => (
+              <li
+                key={language}
+                onClick={() => handleChangeLanguage(language)}
+                className={
+                  language === activeLanguage ? styles['selected-language'] : ''
+                }
+              >
+                {language}
+              </li>
+            ))}
           </ul>
         </div>
 
